74e0101d20492bb5495ce278ce07b262860916c3
James Hoffmann <jamesolinhoffmann@gmail.com>
2021-12-06 20:01:38 -0600

    cleanup

--- day6/part2.js
+++ day6/part2.js
@@ -1,46 +1,32 @@
 import {readFile} from '../readfile.js'
 
 const NUM_DAYS = 256
-let adults = []
-let babies = []
+let fishies = []
 
 const main = () => {
     parseInput()
-    solve()
+    dayCycle()
     console.log(getTotal())
 }
 
 const parseInput = () => {
-    adults = new Array(7).fill(0)
-    babies = new Array(9).fill(0)
-    readFile().split(',').forEach((d) => adults[d] += 1)
+    fishies = new Array(9).fill(0)
+    readFile().split(',').forEach((d) => fishies[d] += 1)
 }
 
-const solve = () => {
+const dayCycle = () => {
     for (let d = 1; d <= NUM_DAYS; d++) { 
-        const tempAdults = adults.map(el => el)
-        const tempBabies = babies.map(el => el)
-        for (let i = 0; i < 8; i++) {
-            if (i < 6) {
-                adults[i] = tempAdults[i+1]
-                babies[i] = tempBabies[i+1]
-            } else if (i === 6) {
-                adults[6] = 0
-                adults[6] += tempAdults[0] + tempBabies[0]
-                babies[8] = 0
-                babies[8] += tempAdults[0] + tempBabies[0]
-                babies[6] = tempBabies[7]
-            } else if (i === 7) {
-                babies[7] = tempBabies[8]
-            }
-        }
+        const prevFishies = fishies.map(el => el)
+        for (let i = 0; i < 6; i++) fishies[i] = prevFishies[i+1]
+        fishies[6] = prevFishies[7] + prevFishies[0]
+        fishies[7] = prevFishies[8]
+        fishies[8] = prevFishies[0]
     }
 }
 
 const getTotal = () => {
     let total = 0
-    adults.forEach(el => total += el)
-    babies.forEach(el => total += el)
+    fishies.forEach(el => total += el)
     return total
 }
 
